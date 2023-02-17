f65935dc06839b664369b35c82163755f46340d0
Nasser <nasser.kessas@outlook.com>
2023-02-17 15:04:36 +1000

    Improved printBlock function

--- hamming.c
+++ hamming.c
@@ -1,23 +1,48 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-char *block2bin(unsigned short i);
+#define block unsigned short
+
+int decode(block input[] );
+void printBlock(block i);
 
 int main () {
-    unsigned short bits = 0b0010101110001110;
-    printf("bits: %s\n", block2bin(bits));
+    block bits = 0b0010101110001110;
+    block input[1]  = {bits};
+    decode(input);
+    return 0;
 }
 
-char *block2bin(unsigned short i) {
-    size_t bits = sizeof(short) * sizeof(char) * 8;
+int decode(block input[] ) {
+    printBlock(input[0]);
+}
 
-    char * str = malloc(bits + 1);
+
+void printBlock(block i) {
+    size_t size = sizeof(short) * sizeof(char) * 8;
+    size_t bit = size;
+
+    char * str = malloc(size + 1);
     if(!str) return NULL;
-    str[bits] = 0;
+    str[size] = 0;
 
     unsigned u = *(unsigned *)&i;
-    for(; bits--; u >>= 1)
-        str[bits] = u & 1 ? '1' : '0';
+    for(; bit--; u >>= 1)
+        str[bit] = u & 1 ? '1' : '0';
 
-    return str;
+    for (size_t i = 0; i < size; i++) {
+        putchar(str[i]);
+        putchar(' ');
+        if (i % 4 == 3) {
+            putchar('\n');
+        }
+    }
+
+    // for (int i = 0; i < sizeof(block)*2; i+=4) {
+    //     for (int j=0; i<4; j++) {
+    //         putchar(str[i+j]);
+    //     }
+    //     putchar('\n');
+
+    // }
 }
