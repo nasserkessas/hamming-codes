54e5c9d4a88f64ebb3f32fe6d32a8e9c319b8003
Nasser <nasser.kessas@outlook.com>
2023-02-17 15:20:32 +1000

    Added getBit function and bit type

--- hamming.c
+++ hamming.c
@@ -1,10 +1,13 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #define block unsigned short
+#define bit bool
 
-int decode(block input[] );
+int decode(block input[]);
 void printBlock(block i);
+bit getBit(block b, int i);
 
 int main () {
     block bits = 0b0010101110001110;
@@ -15,20 +18,25 @@ int main () {
 
 int decode(block input[] ) {
     printBlock(input[0]);
+    int index = 0;
+    printf("%d\n", getBit(input[0], index));
 }
 
+bit getBit(block b, int i) {
+    return (b >> i) & 1;
+}
 
 void printBlock(block i) {
     size_t size = sizeof(short) * sizeof(char) * 8;
-    size_t bit = size;
+    size_t current_bit = size;
 
     char * str = malloc(size + 1);
     if(!str) return NULL;
     str[size] = 0;
 
     unsigned u = *(unsigned *)&i;
-    for(; bit--; u >>= 1)
-        str[bit] = u & 1 ? '1' : '0';
+    for(; current_bit--; u >>= 1)
+        str[current_bit] = u & 1 ? '1' : '0';
 
     for (size_t i = 0; i < size; i++) {
         putchar(str[i]);
